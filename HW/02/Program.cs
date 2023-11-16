1faf27f9c3640e880cc902668ceea7c84cf5eb43
Макс Просто <kirek@mail.ru>
2023-11-17 01:09:34 +0500

    ДЗ 2

---
+++ HW/02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadIn(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    return int.Parse(a);
+}
+
+int SumOfNaturalNumbers(int min, int max)
+{
+    if (max <min){return 0;}
+    return SumOfNaturalNumbers(min+1, max)+min;
+
+}
+int sum = SumOfNaturalNumbers(ReadIn("от"), ReadIn("до"));
+if (sum == 0) {Console.WriteLine("Число \"До\" должно быть больше числа \"От\"." );}
+else {Console.WriteLine(sum);}
+
+    
