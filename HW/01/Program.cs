e2e9d198c08acce0445277f8148ba29d2ab75ca6
Макс Просто <kirek@mail.ru>
2023-11-17 00:40:59 +0500

    ДЗ 1

---
+++ HW/01/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int ReadIn(string msg)
+{
+    Console.WriteLine(msg);
+    string a = Console.ReadLine();
+    return int.Parse(a);
+}
+
+void PrintNaturalNumbers(int min, int max)
+{
+    if (max <min){return;}
+    PrintNaturalNumbers(min, max-2);
+    Console.Write($"{max}\t");
+}
+PrintNaturalNumbers(ReadIn("от"), ReadIn("до"));
