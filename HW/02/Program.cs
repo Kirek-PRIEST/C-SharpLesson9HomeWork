9fdda4b5f8def0749a730fcc862b4f3512ee2702
Макс Просто <kirek@mail.ru>
2023-11-17 21:57:29 +0500

    доработано дз 2

--- HW/02/Program.cs
+++ HW/02/Program.cs
@@ -3,21 +3,23 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-int ReadIn(string msg)
+int ReadIn(string msg)// ввод числа
 {
     Console.WriteLine(msg);
     string a = Console.ReadLine();
     return int.Parse(a);
 }
 
-int SumOfNaturalNumbers(int min, int max)
+int SumOfNaturalNumbers(int min, int max) //Вычисление суммы в заданном диапазоне
 {
-    if (max <min){return 0;}
-    return SumOfNaturalNumbers(min+1, max)+min;
+    if (max < min) { return 0; }
+    return SumOfNaturalNumbers(min + 1, max) + min;
 
 }
-int sum = SumOfNaturalNumbers(ReadIn("от"), ReadIn("до"));
-if (sum == 0) {Console.WriteLine("Число \"До\" должно быть больше числа \"От\"." );}
-else {Console.WriteLine(sum);}
+int M = ReadIn("Введите первое число");
+int N = ReadIn("Введите второе число");
+int sum = 0;
+if (M < 1 || N < 1) { Console.WriteLine("Оба числа долны быть натуральными."); } // Проверка числа на натуральность
+else if (M > N) { Console.WriteLine($"Сумма чисел в диапазоне от {N} до {M} равна {SumOfNaturalNumbers(N, M)}");} //Если второе число больше первого
+else { Console.WriteLine($"Сумма чисел в диапазоне от {M} до {N} равна {SumOfNaturalNumbers(M, N)}");} //Если первое больше ворого
 
-    
