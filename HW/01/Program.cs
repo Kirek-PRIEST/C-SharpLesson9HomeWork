4a01eca61dbd3c408511f2bd2804282b2051acdc
Макс Просто <kirek@mail.ru>
2023-11-17 21:15:40 +0500

    оработано дз 1

--- HW/01/Program.cs
+++ HW/01/Program.cs
@@ -3,17 +3,39 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
-int ReadIn(string msg)
+int ReadIn(string msg) //Ввод числа
 {
     Console.WriteLine(msg);
     string a = Console.ReadLine();
     return int.Parse(a);
 }
 
-void PrintNaturalNumbers(int min, int max)
+void PrintNaturalNumbers(int min, int max) //Вывод положительных чисел в заданном диапазоне
 {
-    if (max <min){return;}
-    PrintNaturalNumbers(min, max-2);
-    Console.Write($"{max}\t");
+    if (max < min) { return; }
+    Console.Write($"{min}\t");
+    PrintNaturalNumbers(min + 2, max);
 }
-PrintNaturalNumbers(ReadIn("от"), ReadIn("до"));
+int minNumber = ReadIn("от");
+int maxNumber = ReadIn("до");
+if (maxNumber == 0 || maxNumber == 0 ){Console.WriteLine("Минимальное число должно быть больше 0.");} // Если введён 0
+else if (minNumber < 0) { Console.WriteLine("Число должно быть натуральным."); } //Если введено отрицательное число
+else if (minNumber == maxNumber && minNumber%2 !=0){Console.WriteLine("В диапазоне нет чётных чисел");} //Если введены два одинаковых нечётных числа
+else if (minNumber > maxNumber) // Если первое введённое число больше второго
+{
+    if (maxNumber % 2 != 0)// Проверка на чётность
+    {
+        maxNumber = maxNumber + 1;
+        Console.WriteLine(maxNumber);
+        PrintNaturalNumbers(maxNumber, minNumber);
+    }
+    else { PrintNaturalNumbers(maxNumber, minNumber); }
+
+}
+else if (minNumber % 2 != 0) // Проверка на чётность
+{
+    minNumber = minNumber + 1;
+    Console.WriteLine(minNumber);
+    PrintNaturalNumbers(minNumber, maxNumber);
+}
+else { PrintNaturalNumbers(minNumber, maxNumber); }
